051a711131defed07be09a72fcd830a769648240
WuYu <yu.wu@patpat.com>
2019-12-05 15:41:57 +0800

    单品商品选择产品页面优化

--- app/Modules/Goods/Resources/Views/single/form.blade.php
+++ app/Modules/Goods/Resources/Views/single/form.blade.php
@@ -3,12 +3,13 @@
     <style>
         .layui-table th, .layui-table tr{text-align: center;}
         #skuTable tbody td{padding: 0;}
+        #skuTable tbody tr{height: 40px;}
         #skuTable .layui-input{border: 0;}
         #skuTable .layui-form-switch{margin-left: 0;}
     </style>
 @endsection
 @section('content')
-    <form class="layui-form layui-form-pane" lay-filter="product">
+    <form class="layui-form layui-form-pane" lay-filter="single">
         <div class="layui-card">
             <div class="layui-card-header">
                 <h3>基本信息</h3>
@@ -57,7 +58,10 @@
                 <table class="layui-table" id="skuTable">
                     <thead>
                     <tr>
-                        <th rowspan="2" width="8%">是否开启</th>
+                        <th rowspan="2" width="8%">
+                            是否开启
+                            <input type="checkbox" name="" lay-skin="switch" lay-text="是|否" switch-index="all" lay-filter="enableSku">
+                        </th>
                         <th colspan="3">产品信息</th>
                         <th rowspan="2" class="required">SKU编号</th>
                         <th rowspan="2" class="required">最低售价</th>
@@ -72,14 +76,14 @@
                     <tbody>
                     @if(isset($product->skus))
                         @foreach($product->skus as $sku)
-                            <tr>
-                                <td><input type="checkbox" name="switch" lay-skin="switch" lay-text="是|否" value="1"></td>
+                            <tr class="layui-disabled" row-index="{{$sku->id}}">
+                                <td><input type="checkbox" name="skus[{{$sku->id}}][enabled]" lay-skin="switch" lay-text="是|否" value="1" switch-index="{{$sku->id}}" lay-filter="enableSku"></td>
                                 <td>{{$sku->code or ''}}</td>
                                 <td>{{(float)$sku->weight ? $sku->weight : ''}}</td>
                                 <td>{{(float)$sku->cost_price ? $sku->weight : ''}}</td>
-                                <td><input type="text" name="skus[{{$sku->id}}][code]" placeholder="SKU编号" lay-verify="required" lay-reqText="请输入SKU编号" class="layui-input" value="{{$sku->code or ''}}"></td>
-                                <td><input type="text" name="skus[{{$sku->id}}][lowest_price]" placeholder="最低售价" lay-verify="required" lay-reqText="请输入最低售价" class="layui-input" value=""></td>
-                                <td><input type="text" name="skus[{{$sku->id}}][msrp]" placeholder="建议零售价" class="layui-input" value=""></td>
+                                <td goods-field="code"></td>
+                                <td goods-field="lowest_price"></td>
+                                <td goods-field="msrp"></td>
                             </tr>
                         @endforeach
                     @endif
@@ -87,15 +91,73 @@
                 </table>
             </div>
         </div>
-        <button type="button" class="layui-btn" lay-submit lay-filter="product">提交</button>
+        <button type="button" class="layui-btn" lay-submit lay-filter="single">提交</button>
         <button type="reset" class="layui-btn layui-btn-primary">重置</button>
     </form>
 @endsection
 @section('scripts')
     <script>
-        layui.use(['form'], function () {
-            var form = layui.form;
+        var productSkus = <?= json_encode(array_column($product->skus->toArray(), null,'id')); ?>;
 
+        layui.use(['form'], function () {
+            var form = layui.form
+                    ,enableSku = function (sku_id, enable) {
+                var $tr = $('tr[row-index=' + sku_id + ']');
+                var productSku = productSkus[sku_id];
+                if (enable) {
+                    $tr.removeClass('layui-disabled');
+                    $tr.find('input[lay-filter=enableSku]').prop('checked', true);
+                    $tr.children('td[goods-field=code]').html('<input type="text" name="skus[' + productSku.id + '][code]" placeholder="SKU编号" lay-verify="required" lay-reqText="请输入SKU编号" class="layui-input" value="' + productSku.code + '">');
+                    $tr.children('td[goods-field=lowest_price]').html('<input type="text" name="skus[' + productSku.id + '][lowest_price]" placeholder="最低售价" lay-verify="required" lay-reqText="请输入最低售价" class="layui-input">');
+                    $tr.children('td[goods-field=msrp]').html('<input type="text" name="skus[' + productSku.id + '][msrp]" placeholder="建议零售价" class="layui-input">');
+                }else {
+                    $tr.addClass('layui-disabled');
+                    $tr.find('input[lay-filter=enableSku]').prop('checked', false);
+                    $tr.children('td[goods-field=code]').html('');
+                    $tr.children('td[goods-field=lowest_price]').html('');
+                    $tr.children('td[goods-field=msrp]').html('');
+                }
+
+                form.render('checkbox', 'single');
+            }
+                    ,inspectAll = function () {
+                var checked = 0, $headerSwitch = $('#skuTable thead input[lay-filter=enableSku]'), $switches = $('#skuTable tbody input[lay-filter=enableSku]');
+                if (0 == $switches.length) {
+                    $headerSwitch.prop('checked', false);
+                }else {
+                    $switches.each(function (key, elem) {
+                        if ($(elem).prop('checked')) {
+                            checked += 1;
+                        }
+                    });
+                    if (checked == $switches.length) {
+                        $headerSwitch.prop('checked', true);
+                    }else {
+                        $headerSwitch.prop('checked', false);
+                    }
+                }
+
+                form.render('checkbox', 'single');
+            };
+
+            // 监听开关
+            form.on('switch(enableSku)', function(data){
+                var switchIndex = $(data.elem).attr('switch-index');
+
+                if ('all' == switchIndex) {
+                    $.each(productSkus, function (sku_id, _) {
+                        enableSku(sku_id, data.elem.checked);
+                    });
+                }else {
+                    enableSku(switchIndex, data.elem.checked);
+                    inspectAll();
+                }
+            });
+
+            // 表单提交
+            form.on('submit(single)', function (form_data) {
+
+            });
         });
     </script>
 @endsection
