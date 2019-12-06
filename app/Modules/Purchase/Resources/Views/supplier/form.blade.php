a26a6c80ab9d0b65116598eb0f3bc65a9f4aa1ac
wuyu <492444775@qq.com>
2019-12-06 23:22:55 +0800

    供应商表单页面

--- app/Modules/Purchase/Resources/Views/supplier/form.blade.php
+++ app/Modules/Purchase/Resources/Views/supplier/form.blade.php
@@ -1,1 +1,147 @@
 @extends('layouts.default')
+@section('css')
+    <style>
+        .layui-table th, .layui-table tr{text-align: center;}
+        #contactTable tbody td{padding: 0;}
+        #contactTable tbody tr{height: 40px;}
+        #contactTable .layui-input{border: 0;}
+        .layui-form-select .layui-input:not(:first-child) {
+            border-left: 0;
+        }
+    </style>
+@endsection
+@section('content')
+    <form class="layui-form layui-form-pane" lay-filter="supplier">
+        <div class="layui-card">
+            <div class="layui-card-header">
+                <h3>基本信息</h3>
+            </div>
+            <div class="layui-card-body">
+                <div class="layui-row layui-col-space30">
+                    <div class="layui-col-xs4">
+                        <div class="layui-form-item">
+                            <label class="layui-form-label required">名称</label>
+                            <div class="layui-input-block">
+                                <input type="text" name="code" lay-verify="required" lay-reqText="请输入名称" class="layui-input" value="">
+                            </div>
+                        </div>
+                        <div class="layui-form-item">
+                            <label class="layui-form-label required">编号</label>
+                            <div class="layui-input-block">
+                                <input type="text" name="code" lay-verify="required" lay-reqText="请输入编号" class="layui-input" value="">
+                            </div>
+                        </div>
+                        <div class="layui-form-item">
+                            <label class="layui-form-label">公司</label>
+                            <div class="layui-input-block">
+                                <input type="text" name="company" class="layui-input" value="">
+                            </div>
+                        </div>
+                        <div class="layui-form-item">
+                            <label class="layui-form-label">电话</label>
+                            <div class="layui-input-block">
+                                <input type="text" name="phone" class="layui-input" value="">
+                            </div>
+                        </div>
+                        <div class="layui-form-item">
+                            <label class="layui-form-label">传真</label>
+                            <div class="layui-input-block">
+                                <input type="text" name="fax" class="layui-input" value="">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="layui-col-xs4">
+                        <div class="layui-form-item">
+                            <label class="layui-form-label required">国家</label>
+                            <div class="layui-input-block">
+                                <select name="country" lay-search="" lay-verify="required" lay-reqText="请选择国家">
+                                    <option value="">请选择国家</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="layui-form-item">
+                            <label class="layui-form-label">地址</label>
+                            <div class="layui-input-block">
+                                <div class="layui-col-xs4">
+                                    <select name="" lay-search="" lay-verify="required" lay-reqText="请选择分类">
+                                        <option value="">请选择省份</option>
+                                    </select>
+                                </div>
+                                <div class="layui-col-xs4">
+                                    <select name="" lay-search="" lay-verify="required" lay-reqText="请选择分类">
+                                        <option value="">请选择市</option>
+                                    </select>
+                                </div>
+                                <div class="layui-col-xs4">
+                                    <select name="" lay-search="" lay-verify="required" lay-reqText="请选择分类">
+                                        <option value="">请选择县/区</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="layui-form-item layui-form-text">
+                            <label class="layui-form-label">简介</label>
+                            <div class="layui-input-block">
+                                <textarea name="desc" class="layui-textarea"></textarea>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="layui-card">
+            <div class="layui-card-header">
+                <h3>联系人</h3>
+            </div>
+            <div class="layui-card-body">
+                <table class="layui-table" id="contactTable">
+                    <thead>
+                    <tr>
+                        <th class="required">名称</th>
+                        <th>职位</th>
+                        <th>电话</th>
+                        <th>操作</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+
+                    </tbody>
+                </table>
+            </div>
+            <div class="layui-card-body">
+                <button type="button" class="layui-btn layui-btn-normal" lay-event="addContact">添加联系人</button>
+            </div>
+        </div>
+        <button type="button" class="layui-btn" lay-submit lay-filter="supplier">提交</button>
+        <button type="reset" class="layui-btn layui-btn-primary">重置</button>
+    </form>
+@endsection
+@section('scripts')
+    <script>
+        layui.use(['form'], function () {
+            var form = layui.form;
+            $('button[lay-event=addContact]').on('click', function () {
+                var $body = $('#contactTable').find('tbody')
+                        ,html = ''
+                        ,sku_flag = Date.now();
+                html += '<tr>';
+                html += '<td>';
+                html += '<input type="text" name="contacts[' + sku_flag + '][name]" placeholder="名称" lay-verify="required" lay-reqText="请输入名称" class="layui-input">';
+                html += '</td>';
+                html += '<td>';
+                html += '<input type="text" name="contacts[' + sku_flag + '][position]" placeholder="职位" class="layui-input">';
+                html += '</td>';
+                html += '<td>';
+                html += '<input type="text" name="contacts[' + sku_flag + '][phone]" placeholder="电话" class="layui-input">';
+                html += '</td>';
+                html += '<td>';
+                html += '<button type="button" class="layui-btn layui-btn-sm layui-btn-danger" onclick="deleteRow(this);">删除</button>';
+                html += '</td>';
+                html += '</tr>';
+
+                $body.append(html);
+                form.render();
+            });
+        });
+    </script>
+@endsection
