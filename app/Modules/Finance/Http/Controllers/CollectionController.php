95c6f9eb0787e19243165ce73cb70ae3e39b4c20
WuYu <yu.wu@patpat.com>
2019-12-26 15:05:51 +0800

    收款单联动效果

--- app/Modules/Finance/Http/Controllers/CollectionController.php
+++ app/Modules/Finance/Http/Controllers/CollectionController.php
@@ -4,7 +4,9 @@ namespace App\Modules\Finance\Http\Controllers;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Modules\Index\Models\User;
 use App\Modules\Sale\Models\Customer;
+use App\Modules\Finance\Models\Account;
 
 class CollectionController extends Controller
 {
@@ -20,8 +22,10 @@ class CollectionController extends Controller
 
     public function form(Request $request)
     {
+        $users = User::where('is_admin', 0)->get();
         $customer = Customer::find($request->get('customer_id'));
+        $accounts = Account::all();
 
-        return view('finance::collection.form', compact('customer'));
+        return view('finance::collection.form', compact('users', 'customer', 'accounts'));
     }
 }
--- app/Modules/Finance/Resources/Views/collection/form.blade.php
+++ app/Modules/Finance/Resources/Views/collection/form.blade.php
@@ -25,6 +25,39 @@
                                 <input type="text" name="amount" lay-verify="required" lay-reqText="请输入收款金额" class="layui-input" value="" oninput="value=value.replace(/[^\d.]/g, '')">
                             </div>
                         </div>
+                        <div class="layui-form-item">
+                            <label class="layui-form-label required">收款方式</label>
+                            <div class="layui-input-block">
+                                <select name="method" lay-filter="method" lay-verify="required" lay-reqText="请选择收款方式">
+                                    <option value="">请选择收款方式</option>
+                                    <option value="1">现金</option>
+                                    <option value="2">汇款</option>
+                                    <option value="3">支票/汇票</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="layui-form-item layui-hide">
+                            <label class="layui-form-label required">收款人</label>
+                            <div class="layui-input-block">
+                                <select name="collect_user_id" lay-verify="required" lay-reqText="请选择收款人">
+                                    <option value="">请选择收款人</option>
+                                    @foreach($users as $user)
+                                        <option value="{{$user->id}}">{{$user->name}}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                        </div>
+                        <div class="layui-form-item layui-hide">
+                            <label class="layui-form-label required">汇款账户</label>
+                            <div class="layui-input-block">
+                                <select name="remit_account" lay-verify="required" lay-reqText="请选择汇款账户">
+                                    <option value="">请选择汇款账户</option>
+                                    @foreach($accounts as $account)
+                                        <option value="{{$account->id}}">{{$account->name}}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -81,6 +114,28 @@
                     layer.msg("选中的明细金额不可大于收款金额", {icon: 5, shift: 6});
                     return false;
                 }
+            });
+
+            form.on('select(method)', function (data) {
+                var $collectUserSelect = $('select[name=collect_user_id]')
+                        ,$remitAccountSelect = $('select[name=remit_account]')
+                        ,$collectUserItem = $collectUserSelect.parents('.layui-form-item')
+                        ,$remitAccountItem = $remitAccountSelect.parents('.layui-form-item');
+
+                $collectUserSelect.val('');
+                $remitAccountSelect.val('');
+                if (!data.value || 3 == data.value) {
+                    $collectUserItem.addClass('layui-hide');
+                    $remitAccountItem.addClass('layui-hide');
+                }else if (1 == data.value) {
+                    $collectUserItem.removeClass('layui-hide');
+                    $remitAccountItem.addClass('layui-hide');
+                }else if (2 == data.value) {
+                    $collectUserItem.addClass('layui-hide');
+                    $remitAccountItem.removeClass('layui-hide');
+                }
+
+                form.render('select', 'collection');
             });
 
             // 提交收款单
