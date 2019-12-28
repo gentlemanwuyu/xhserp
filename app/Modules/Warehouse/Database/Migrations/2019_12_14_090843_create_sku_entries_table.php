c9065b0e7ab4710ff1f9123b3acbb48744789695
WuYu <yu.wu@patpat.com>
2019-12-28 17:58:21 +0800

    添加is_paid字段

--- app/Modules/Warehouse/Database/Migrations/2019_12_14_090843_create_sku_entries_table.php
+++ app/Modules/Warehouse/Database/Migrations/2019_12_14_090843_create_sku_entries_table.php
@@ -17,6 +17,7 @@ class CreateSkuEntriesTable extends Migration
 			$table->integer('sku_id')->default(0)->comment('SkuID');
 			$table->integer('order_item_id')->default(0)->comment('订单itemID');
 			$table->integer('quantity')->default(0)->comment('入库数量');
+			$table->enum('is_paid', [0, 1])->default(0)->comment('是否已付款, 1为已付款');
 			$table->integer('user_id')->default(0)->comment('操作人ID');
 			$table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'))->comment('创建时间');
 			$table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'))->comment('最后更新时间');
