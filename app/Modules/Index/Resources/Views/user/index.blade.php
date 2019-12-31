00d72c02265fba6b34d954776c2f77f9488a0530
WuYu <yu.wu@patpat.com>
2019-12-31 12:01:55 +0800

    用户列表页修改

--- app/Modules/Index/Resources/Views/user/index.blade.php
+++ app/Modules/Index/Resources/Views/user/index.blade.php
@@ -1,16 +1,20 @@
 @extends('layouts.default')
 @section('content')
-    <a class="layui-btn layui-btn-sm layui-btn-normal" lay-href="{{route('index::user.form')}}">添加用户</a>
+    <a class="layui-btn layui-btn-normal" lay-href="{{route('index::user.form')}}">添加用户</a>
     <table id="list"  lay-filter="list"></table>
     <script type="text/html" id="action">
-        <a class="layui-btn layui-btn-sm layui-btn-normal" lay-event="edit">编辑</a>
-        <a class="layui-btn layui-btn-sm layui-btn-danger" lay-event="delete">删除</a>
+        <div class="urp-dropdown urp-dropdown-table" title="操作">
+            <i class="layui-icon layui-icon-more-vertical urp-dropdown-btn"></i>
+        </div>
     </script>
 @endsection
 @section('scripts')
     <script>
-        layui.use(['table'], function(){
+        layui.extend({
+            dropdown: '/assets/layui-table-dropdown/dropdown'
+        }).use(['table', 'dropdown'], function(){
             var table = layui.table
+                    ,dropdown = layui.dropdown
                     ,tableIns = table.render({
                 elem: '#list',
                 url: "{{route('index::user.paginate')}}",
@@ -32,39 +36,49 @@
                     {field: 'telephone', title: '电话', align: 'center'},
                     {field: 'created_at', title: '创建时间', align: 'center', sort: true},
                     {field: 'updated_at', title: '最后更新时间', align: 'center', sort: true},
-                    {field: 'action', title: '操作', width: '15%', align: 'center', toolbar: "#action"}
+                    {field: 'action', title: '操作', width: 100, align: 'center', toolbar: "#action"}
                 ]]
-            });
-
-            table.on('tool(list)', function(obj){
-                var data = obj.data;
-
-                if ('edit' == obj.event) {
-                    parent.layui.index.openTabsPage("{{route('index::user.form')}}?user_id=" + data.id, '编辑用户[' + data.id + ']');
-                }else if ('delete' == obj.event) {
-                    layer.confirm("确认要删除该用户？", {icon: 3, title:"确认"}, function (index) {
-                        layer.close(index);
-                        var load_index = layer.load();
-                        $.ajax({
-                            method: "post",
-                            url: "{{route('index::user.delete')}}",
-                            data: {user_id: data.id},
-                            success: function (data) {
-                                layer.close(load_index);
-                                if ('success' == data.status) {
-                                    layer.msg("用户删除成功", {icon:1});
-                                    tableIns.reload();
-                                } else {
-                                    layer.msg("用户删除失败:"+data.msg, {icon:2});
-                                    return false;
-                                }
-                            },
-                            error: function (XMLHttpRequest, textStatus, errorThrown) {
-                                layer.close(load_index);
-                                layer.msg(packageValidatorResponseText(XMLHttpRequest.responseText), {icon:2});
-                                return false;
+                ,done: function(res, curr, count){
+                    dropdown(res.data,function(data) {
+                        var actions = [];
+                        actions.push({
+                            title: "编辑",
+                            event: function () {
+                                parent.layui.index.openTabsPage("{{route('index::user.form')}}?user_id=" + data.id, '编辑用户[' + data.id + ']');
                             }
                         });
+
+                        actions.push({
+                            title: "删除",
+                            event: function() {
+                                layer.confirm("确认要删除该用户？", {icon: 3, title:"确认"}, function (index) {
+                                    layer.close(index);
+                                    var load_index = layer.load();
+                                    $.ajax({
+                                        method: "post",
+                                        url: "{{route('index::user.delete')}}",
+                                        data: {user_id: data.id},
+                                        success: function (data) {
+                                            layer.close(load_index);
+                                            if ('success' == data.status) {
+                                                layer.msg("用户删除成功", {icon:1});
+                                                tableIns.reload();
+                                            } else {
+                                                layer.msg("用户删除失败:"+data.msg, {icon:2});
+                                                return false;
+                                            }
+                                        },
+                                        error: function (XMLHttpRequest, textStatus, errorThrown) {
+                                            layer.close(load_index);
+                                            layer.msg(packageValidatorResponseText(XMLHttpRequest.responseText), {icon:2});
+                                            return false;
+                                        }
+                                    });
+                                });
+                            }
+                        });
+
+                        return actions;
                     });
                 }
             });
