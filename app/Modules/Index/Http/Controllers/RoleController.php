a904302e5c5e28a5268844bdadff4b39d6c7c3b9
WuYu <yu.wu@patpat.com>
2020-04-13 15:46:59 +0800

    分配权限

--- app/Modules/Index/Http/Controllers/RoleController.php
+++ app/Modules/Index/Http/Controllers/RoleController.php
@@ -4,7 +4,9 @@ namespace App\Modules\Index\Http\Controllers;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use App\Modules\Index\Models\Role;
+use App\Modules\Index\Models\Permission;
 
 class RoleController extends Controller
 {
@@ -36,10 +38,15 @@ class RoleController extends Controller
 
     public function form(Request $request)
     {
-        $data = [];
+        $tree = Permission::tree();
+        $data = compact('tree');
+        $permissions = [];
         if ($request->get('role_id')) {
-            $data['role'] = Role::find($request->get('role_id'));
+            $role = Role::find($request->get('role_id'));
+            $data['role'] = $role;
+            $permissions = array_unique(array_column($role->permissions->toArray(), 'name'));
         }
+        $data['permissions'] = $permissions;
 
         return view('index::role.form', $data);
     }
@@ -52,10 +59,23 @@ class RoleController extends Controller
                 'display_name' => $request->get('display_name', ''),
             ];
 
-            Role::updateOrCreate(['id' => $request->get('role_id')], $data);
+            $name_exist = Role::where('name', $request->get('name'));
+            if ($request->get('role_id')) {
+                $name_exist->where('id', '!=', $request->get('role_id'));
+            }
+            if ($name_exist->first()) {
+                throw new \Exception("角色名已存在");
+            }
 
+            DB::beginTransaction();
+            $role = Role::updateOrCreate(['id' => $request->get('role_id')], $data);
+            $permissions = $request->get('permissions') ? array_values($request->get('permissions')) : [];
+            $role->syncPermissions($permissions);
+
+            DB::commit();
             return response()->json(['status' => 'success']);
         }catch (\Exception $e) {
+            DB::rollBack();
             return response()->json(['status' => 'fail', 'msg' => $e->getMessage(), 'exception' => get_class($e)]);
         }
     }
--- app/Modules/Index/Resources/Views/role/form.blade.php
+++ app/Modules/Index/Resources/Views/role/form.blade.php
@@ -1,4 +1,13 @@
 @extends('layouts.default')
+@section('css')
+    <style>
+        .layui-card[erp-level_1]{box-shadow: none;border: 1px solid #F2F2F2;}
+        .layui-card[erp-level_1]>.layui-card-header{background-color: #F2F2F2;}
+        .layui-card[erp-level_2]{margin-bottom: 0;box-shadow: none;}
+        .layui-card[erp-level_2]>.layui-card-header{border-bottom: 0;}
+        .layui-card[erp-level_2]>.layui-card-body{padding-left: 35px;}
+    </style>
+@endsection
 @section('content')
     <form class="layui-form layui-form-pane" lay-filter="role">
         @if(isset($role_id))
@@ -27,6 +36,41 @@
                 </div>
             </div>
         </div>
+        @if(!empty($tree))
+            <div class="layui-card" id="assign_permission">
+                <div class="layui-card-header">
+                    <h3 style="display: inline;">分配权限</h3>
+                    <a href="javascript:;" erp-event="assign_all" data-value="1">[全选]</a>
+                </div>
+                <div class="layui-card-body">
+                    @foreach($tree as $p1)
+                        <div class="layui-card" erp-level_1>
+                            <div class="layui-card-header">
+                                <h3><input type="checkbox" name="permissions[]" value="{{$p1['name']}}" title="{{$p1['display_name']}}" lay-filter="permission" lay-skin="primary" erp-level="1" @if(in_array($p1['name'], $permissions)) checked @endif></h3>
+                            </div>
+                            @if(!empty($p1['children']))
+                                <div class="layui-card-body">
+                                    @foreach($p1['children'] as $p2)
+                                        <div class="layui-card" erp-level_2>
+                                            <div class="layui-card-header">
+                                                <h3><input type="checkbox" name="permissions[]" value="{{$p2['name']}}" title="{{$p2['display_name']}}" lay-filter="permission" lay-skin="primary" erp-level="2" @if(in_array($p2['name'], $permissions)) checked @endif></h3>
+                                            </div>
+                                            @if(!empty($p2['children']))
+                                                <div class="layui-card-body">
+                                                    @foreach($p2['children'] as $p3)
+                                                        <input type="checkbox" name="permissions[]" value="{{$p3['name']}}" title="{{$p3['display_name']}}" lay-filter="permission" lay-skin="primary" erp-level="3" @if(in_array($p3['name'], $permissions)) checked @endif>
+                                                    @endforeach
+                                                </div>
+                                            @endif
+                                        </div>
+                                    @endforeach
+                                </div>
+                            @endif
+                        </div>
+                    @endforeach
+                </div>
+            </div>
+        @endif
         <button type="button" class="layui-btn" lay-submit lay-filter="role">提交</button>
         <button type="reset" class="layui-btn layui-btn-primary">重置</button>
     </form>
@@ -35,6 +79,37 @@
     <script>
         layui.use(['form'], function () {
             var form = layui.form;
+
+            form.on('checkbox(permission)', function(data){
+                var $checkbox = $(data.elem)
+                        ,level = $checkbox.attr('erp-level')
+                        ,checked = data.elem.checked;
+
+                if (1 == level) {
+                    var $card = $checkbox.parents('.layui-card[erp-level_1]');
+                    $card.find('.layui-card-body input[lay-filter=permission]').prop('checked', checked);
+                }else if (2 == level) {
+                    var $card = $checkbox.parents('.layui-card[erp-level_2]');
+                    $card.find('.layui-card-body input[lay-filter=permission]').prop('checked', checked);
+                }
+
+                form.render('checkbox');
+            });
+
+            $('*[erp-event=assign_all]').on('click', function () {
+                var value = $(this).attr('data-value')
+                        ,$card = $(this).parents('.layui-card');
+                if (1 == value) {
+                    $card.find('input[lay-filter=permission]').prop('checked', true);
+                    $(this).html('[全不选]');
+                    $(this).attr('data-value', 0);
+                }else {
+                    $card.find('input[lay-filter=permission]').prop('checked', false);
+                    $(this).html('[全选]');
+                    $(this).attr('data-value', 1);
+                }
+                form.render('checkbox');
+            });
 
             form.on('submit(role)', function (form_data) {
                 var load_index = layer.load();
