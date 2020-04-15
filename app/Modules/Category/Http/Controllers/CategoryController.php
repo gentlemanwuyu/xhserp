35d2e7f679b2286f69d7ee5de90da5449e1b44d4
WuYu <yu.wu@patpat.com>
2020-04-15 13:39:54 +0800

    分类删除时，判断是否有产品/商品

--- app/Modules/Category/Http/Controllers/CategoryController.php
+++ app/Modules/Category/Http/Controllers/CategoryController.php
@@ -5,6 +5,8 @@ use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Modules\Category\Http\Requests\CategoryRequest;
+use App\Modules\Goods\Models\Goods;
+use App\Modules\Product\Models\Product;
 use App\Modules\Category\Models\Category;
 
 class CategoryController extends Controller
@@ -55,7 +57,15 @@ class CategoryController extends Controller
             $category = Category::find($request->get('category_id'));
 
             if (!$category->children->isEmpty()) {
-                return response()->json(['status' => 'fail', 'msg' => '该分类下还有子分类']);
+                throw new \Exception("该分类下还有子分类");
+            }
+
+            $category_ids = array_unique(array_merge([$category->id], $category->children_ids));
+
+            if (1 == $category->type && Product::whereIn('category_id', $category_ids)->exists()) {
+                throw new \Exception("该分类或子分类下还有产品，不能删除");
+            }elseif (2 == $category->type && Goods::whereIn('category_id', $category_ids)->exists()) {
+                throw new \Exception("该分类或子分类下还有商品，不能删除");
             }
 
             $category->delete();
