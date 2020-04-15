f03b3da657ff670f04966a6f82a709d8bedf71a7
WuYu <yu.wu@patpat.com>
2020-04-15 11:50:54 +0800

    商品列表页面分类搜索框改成下拉树

--- app/Modules/Goods/Http/Controllers/GoodsController.php
+++ app/Modules/Goods/Http/Controllers/GoodsController.php
@@ -31,8 +31,14 @@ class GoodsController extends Controller
         if ($request->get('name')) {
             $query = $query->where('name', 'like', '%' . $request->get('name') . '%');
         }
-        if ($request->get('category_id')) {
-            $query = $query->where('category_id', $request->get('category_id'));
+        if ($request->get('category_ids')) {
+            $category_ids = explode(',', $request->get('category_ids'));
+            foreach ($category_ids as $category_id) {
+                $category = Category::find($category_id);
+                $category_ids = array_merge($category_ids, $category->children_ids);
+            }
+
+            $query = $query->whereIn('category_id', array_unique($category_ids));
         }
         if ($request->get('type')) {
             $query = $query->where('type', $request->get('type'));
