30cfd075a4fdb17389ccbccf0600497d6f3058bc
WuYu <yu.wu@patpat.com>
2020-05-15 16:15:12 +0800

    数据填充1

--- app/Modules/Category/Database/Seeds/CategoriesTableSeeder.php
+++ app/Modules/Category/Database/Seeds/CategoriesTableSeeder.php
@@ -28,58 +28,50 @@ class CategoriesTableSeeder extends Seeder
 
         // 液位开关
         $level_switch = Category::create(['name' => '液位开关', 'type' => Category::PRODUCT]);
-        $vertical_switch = Category::create(['name' => '立式液位开关', 'parent_id' => $level_switch->id, 'type' => Category::PRODUCT]);
-        Category::create(['name' => 'PP立式液位开关', 'parent_id' => $vertical_switch->id, 'type' => Category::PRODUCT]);
-        Category::create(['name' => 'PVDF立式液位开关', 'parent_id' => $vertical_switch->id, 'type' => Category::PRODUCT]);
-        Category::create(['name' => '304立式液位开关', 'parent_id' => $vertical_switch->id, 'type' => Category::PRODUCT]);
-        $horizontal_switch = Category::create(['name' => '卧式液位开关', 'parent_id' => $level_switch->id, 'type' => Category::PRODUCT]);
-        Category::create(['name' => 'PP卧式液位开关', 'parent_id' => $horizontal_switch->id, 'type' => Category::PRODUCT]);
-        Category::create(['name' => 'PVDF卧式液位开关', 'parent_id' => $horizontal_switch->id, 'type' => Category::PRODUCT]);
-        Category::create(['name' => '304卧式液位开关', 'parent_id' => $horizontal_switch->id, 'type' => Category::PRODUCT]);
+        Category::create(['name' => '立式液位开关', 'parent_id' => $level_switch->id, 'type' => Category::PRODUCT]);
+        Category::create(['name' => '卧式液位开关', 'parent_id' => $level_switch->id, 'type' => Category::PRODUCT]);
 
         //流量计配件
         $flowmeter_parts = Category::create(['name' => '流量计配件', 'type' => Category::PRODUCT]);
-        $tube = Category::create(['name' => '视管', 'parent_id' => $flowmeter_parts->id, 'type' => Category::PRODUCT]);
-        Category::create(['name' => 'PC视管', 'parent_id' => $tube->id, 'type' => Category::PRODUCT]);
-        Category::create(['name' => 'PVC视管', 'parent_id' => $tube->id, 'type' => Category::PRODUCT]);
-        Category::create(['name' => 'PSU视管', 'parent_id' => $tube->id, 'type' => Category::PRODUCT]);
-        $rotor = Category::create(['name' => '转子', 'parent_id' => $flowmeter_parts->id, 'type' => Category::PRODUCT]);
-        Category::create(['name' => '304转子', 'parent_id' => $rotor->id, 'type' => Category::PRODUCT]);
-        Category::create(['name' => '316转子', 'parent_id' => $rotor->id, 'type' => Category::PRODUCT]);
-        Category::create(['name' => '铁氟龙转子', 'parent_id' => $rotor->id, 'type' => Category::PRODUCT]);
-        Category::create(['name' => '钛转子', 'parent_id' => $rotor->id, 'type' => Category::PRODUCT]);
-        $guide = Category::create(['name' => '导轨', 'parent_id' => $flowmeter_parts->id, 'type' => Category::PRODUCT]);
-        Category::create(['name' => '316导轨', 'parent_id' => $guide->id, 'type' => Category::PRODUCT]);
-        Category::create(['name' => '碳纤棒导轨', 'parent_id' => $guide->id, 'type' => Category::PRODUCT]);
+        Category::create(['name' => '视管', 'parent_id' => $flowmeter_parts->id, 'type' => Category::PRODUCT]);
+        Category::create(['name' => '转子', 'parent_id' => $flowmeter_parts->id, 'type' => Category::PRODUCT]);
+        Category::create(['name' => '导轨', 'parent_id' => $flowmeter_parts->id, 'type' => Category::PRODUCT]);
+        Category::create(['name' => '接头', 'parent_id' => $flowmeter_parts->id, 'type' => Category::PRODUCT]);
         Category::create(['name' => '螺母', 'parent_id' => $flowmeter_parts->id, 'type' => Category::PRODUCT]);
-        $joint = Category::create(['name' => '接头', 'parent_id' => $flowmeter_parts->id, 'type' => Category::PRODUCT]);
-        Category::create(['name' => 'PVC接头', 'parent_id' => $joint->id, 'type' => Category::PRODUCT]);
-        Category::create(['name' => 'PP接头', 'parent_id' => $joint->id, 'type' => Category::PRODUCT]);
         Category::create(['name' => '指示扣', 'parent_id' => $flowmeter_parts->id, 'type' => Category::PRODUCT]);
-        $apron = Category::create(['name' => '流量计胶圈', 'parent_id' => $flowmeter_parts->id, 'type' => Category::PRODUCT]);
-        Category::create(['name' => 'EPDM胶圈', 'parent_id' => $apron->id, 'type' => Category::PRODUCT]);
-        Category::create(['name' => '铁氟龙胶圈', 'parent_id' => $apron->id, 'type' => Category::PRODUCT]);
+        Category::create(['name' => '胶圈', 'parent_id' => $flowmeter_parts->id, 'type' => Category::PRODUCT]);
 
         // 滚轮片
-        $disk = Category::create(['name' => '滚轮片', 'type' => Category::PRODUCT]);
-        Category::create(['name' => 'PP滚轮片', 'parent_id' => $disk->id, 'type' => Category::PRODUCT]);
+        $disk = Category::create(['name' => '行辘片', 'type' => Category::PRODUCT]);
+        $category = Category::create(['name' => '一体滚轮片', 'parent_id' => $disk->id, 'type' => Category::PRODUCT]);
+        Category::create(['name' => 'PP滚轮片', 'parent_id' => $category->id, 'type' => Category::PRODUCT]);
+        Category::create(['name' => '孟山都滚轮片', 'parent_id' => $category->id, 'type' => Category::PRODUCT]);
+        Category::create(['name' => '耐高温滚轮片', 'parent_id' => $category->id, 'type' => Category::PRODUCT]);
         $category = Category::create(['name' => '包胶滚轮片', 'parent_id' => $disk->id, 'type' => Category::PRODUCT]);
         Category::create(['name' => '包胶滚轮片骨架', 'parent_id' => $category->id, 'type' => Category::PRODUCT]);
         Category::create(['name' => '包胶滚轮片胶圈', 'parent_id' => $category->id, 'type' => Category::PRODUCT]);
-        Category::create(['name' => '孟山都滚轮片', 'parent_id' => $disk->id, 'type' => Category::PRODUCT]);
-        Category::create(['name' => '耐高温滚轮片', 'parent_id' => $disk->id, 'type' => Category::PRODUCT]);
-        Category::create(['name' => 'LCD滚轮', 'parent_id' => $disk->id, 'type' => Category::PRODUCT]);
+        $category = Category::create(['name' => 'LCD滚轮', 'parent_id' => $disk->id, 'type' => Category::PRODUCT]);
+        Category::create(['name' => 'LCD滚轮内套', 'parent_id' => $category->id, 'type' => Category::PRODUCT]);
+        Category::create(['name' => 'LCD滚轮外套', 'parent_id' => $category->id, 'type' => Category::PRODUCT]);
+        Category::create(['name' => 'LCD滚轮胶圈', 'parent_id' => $category->id, 'type' => Category::PRODUCT]);
 
-        // 连接套
-        $connect = Category::create(['name' => '连接套', 'type' => Category::PRODUCT]);
-        Category::create(['name' => '卡环', 'parent_id' => $connect->id, 'type' => Category::PRODUCT]);
+        // 间距杆
+        $connect = Category::create(['name' => '间距杆', 'type' => Category::PRODUCT]);
+        Category::create(['name' => '连接套', 'parent_id' => $connect->id, 'type' => Category::PRODUCT]);
+        Category::create(['name' => '数字码', 'parent_id' => $connect->id, 'type' => Category::PRODUCT]);
 
         // 固定套
         $fixed = Category::create(['name' => '固定套', 'type' => Category::PRODUCT]);
+        Category::create(['name' => '外套', 'parent_id' => $fixed->id, 'type' => Category::PRODUCT]);
+        Category::create(['name' => '内套', 'parent_id' => $fixed->id, 'type' => Category::PRODUCT]);
+        Category::create(['name' => '卡环', 'parent_id' => $fixed->id, 'type' => Category::PRODUCT]);
+
+        // 轴套
+        $sleeve = Category::create(['name' => '轴套', 'type' => Category::PRODUCT]);
 
         // 插件
         $cup = Category::create(['name' => '插件', 'type' => Category::PRODUCT]);
-        Category::create(['name' => '卡扣', 'parent_id' => $cup->id, 'type' => Category::PRODUCT]);
+        Category::create(['name' => '限位块', 'parent_id' => $cup->id, 'type' => Category::PRODUCT]);
 
         // 齿轮
         $gear = Category::create(['name' => '齿轮', 'type' => Category::PRODUCT]);
@@ -89,20 +81,23 @@ class CategoriesTableSeeder extends Seeder
         Category::create(['name' => '螺旋齿轮', 'parent_id' => $gear->id, 'type' => Category::PRODUCT]);
         Category::create(['name' => '组合齿轮', 'parent_id' => $gear->id, 'type' => Category::PRODUCT]);
 
+        // 喷管接头
+        $nozzle_joint = Category::create(['name' => '喷管接头', 'type' => Category::PRODUCT]);
+        Category::create(['name' => '外套', 'parent_id' => $nozzle_joint->id, 'type' => Category::PRODUCT]);
+        Category::create(['name' => '内套', 'parent_id' => $nozzle_joint->id, 'type' => Category::PRODUCT]);
+        Category::create(['name' => '胶圈', 'parent_id' => $nozzle_joint->id, 'type' => Category::PRODUCT]);
+
         // 喷咀配件
         $nozzle_parts = Category::create(['name' => '喷咀', 'type' => Category::PRODUCT]);
         Category::create(['name' => '喷咀头', 'parent_id' => $nozzle_parts->id, 'type' => Category::PRODUCT]);
-        Category::create(['name' => '喷嘴芯', 'parent_id' => $nozzle_parts->id, 'type' => Category::PRODUCT]);
+        Category::create(['name' => '喷咀芯', 'parent_id' => $nozzle_parts->id, 'type' => Category::PRODUCT]);
         Category::create(['name' => '底座', 'parent_id' => $nozzle_parts->id, 'type' => Category::PRODUCT]);
-        Category::create(['name' => '叶轮', 'parent_id' => $nozzle_parts->id, 'type' => Category::PRODUCT]);
+        Category::create(['name' => '叶片', 'parent_id' => $nozzle_parts->id, 'type' => Category::PRODUCT]);
 
         // 螺丝
         $screw = Category::create(['name' => '紧固件', 'type' => Category::PRODUCT]);
         Category::create(['name' => '螺丝', 'parent_id' => $screw->id, 'type' => Category::PRODUCT]);
         Category::create(['name' => '螺母', 'parent_id' => $screw->id, 'type' => Category::PRODUCT]);
-
-        // 喷管接口
-        $interface = Category::create(['name' => '喷管接口', 'type' => Category::PRODUCT]);
 
         // 轴心
         $axis = Category::create(['name' => '轴心', 'type' => Category::PRODUCT]);
@@ -114,8 +109,6 @@ class CategoriesTableSeeder extends Seeder
 
         // 胶圈
         $rubber_gasket = Category::create(['name' => '胶圈', 'type' => Category::PRODUCT]);
-        Category::create(['name' => 'EPDM胶圈', 'parent_id' => $rubber_gasket->id, 'type' => Category::PRODUCT]);
-        Category::create(['name' => '铁氟龙胶圈', 'parent_id' => $rubber_gasket->id, 'type' => Category::PRODUCT]);
 
         // 其他配件
         $other = Category::create(['name' => '其他配件', 'type' => Category::PRODUCT]);
@@ -132,14 +125,8 @@ class CategoriesTableSeeder extends Seeder
 
         // 液位开关
         $level_switch = Category::create(['name' => '液位开关', 'type' => Category::GOODS]);
-        $vertical_switch = Category::create(['name' => '立式液位开关', 'parent_id' => $level_switch->id, 'type' => Category::GOODS]);
-        Category::create(['name' => 'PP立式液位开关', 'parent_id' => $vertical_switch->id, 'type' => Category::GOODS]);
-        Category::create(['name' => 'PVDF立式液位开关', 'parent_id' => $vertical_switch->id, 'type' => Category::GOODS]);
-        Category::create(['name' => '304立式液位开关', 'parent_id' => $vertical_switch->id, 'type' => Category::GOODS]);
-        $horizontal_switch = Category::create(['name' => '卧式液位开关', 'parent_id' => $level_switch->id, 'type' => Category::GOODS]);
-        Category::create(['name' => 'PP卧式液位开关', 'parent_id' => $horizontal_switch->id, 'type' => Category::GOODS]);
-        Category::create(['name' => 'PVDF卧式液位开关', 'parent_id' => $horizontal_switch->id, 'type' => Category::GOODS]);
-        Category::create(['name' => '304卧式液位开关', 'parent_id' => $horizontal_switch->id, 'type' => Category::GOODS]);
+        Category::create(['name' => '立式液位开关', 'parent_id' => $level_switch->id, 'type' => Category::GOODS]);
+        Category::create(['name' => '卧式液位开关', 'parent_id' => $level_switch->id, 'type' => Category::GOODS]);
 
         // 流量计
         $flowmeter = Category::create(['name' => '流量计', 'type' => Category::GOODS]);
@@ -155,16 +142,13 @@ class CategoriesTableSeeder extends Seeder
         Category::create(['name' => '耐高温滚轮片', 'parent_id' => $disk->id, 'type' => Category::GOODS]);
         Category::create(['name' => 'LCD滚轮', 'parent_id' => $disk->id, 'type' => Category::GOODS]);
 
-        // 连接套
-        $connect = Category::create(['name' => '连接套', 'type' => Category::GOODS]);
-        Category::create(['name' => '卡环', 'parent_id' => $connect->id, 'type' => Category::GOODS]);
-
         // 固定套
         $fixed = Category::create(['name' => '固定套', 'type' => Category::GOODS]);
 
-        // 插件
-        $cup = Category::create(['name' => '插件', 'type' => Category::GOODS]);
-        Category::create(['name' => '卡扣', 'parent_id' => $cup->id, 'type' => Category::GOODS]);
+        // 间距杆
+        $connect = Category::create(['name' => '间距杆', 'type' => Category::GOODS]);
+        Category::create(['name' => '连接套', 'parent_id' => $connect->id, 'type' => Category::GOODS]);
+        Category::create(['name' => '数字码', 'parent_id' => $connect->id, 'type' => Category::GOODS]);
 
         // 齿轮
         $gear = Category::create(['name' => '齿轮', 'type' => Category::GOODS]);
@@ -177,7 +161,7 @@ class CategoriesTableSeeder extends Seeder
         // 喷咀配件
         $nozzle = Category::create(['name' => '喷咀', 'type' => Category::GOODS]);
         Category::create(['name' => '喷咀', 'parent_id' => $nozzle->id, 'type' => Category::GOODS]);
-        Category::create(['name' => '喷嘴芯', 'parent_id' => $nozzle->id, 'type' => Category::GOODS]);
+        Category::create(['name' => '喷咀芯', 'parent_id' => $nozzle->id, 'type' => Category::GOODS]);
 
         // 螺丝
         $screw = Category::create(['name' => '紧固件', 'type' => Category::GOODS]);
@@ -197,8 +181,6 @@ class CategoriesTableSeeder extends Seeder
 
         // 胶圈
         $rubber_gasket = Category::create(['name' => '胶圈', 'type' => Category::GOODS]);
-        Category::create(['name' => 'EPDM胶圈', 'parent_id' => $rubber_gasket->id, 'type' => Category::GOODS]);
-        Category::create(['name' => '铁氟龙胶圈', 'parent_id' => $rubber_gasket->id, 'type' => Category::GOODS]);
 
         // 滚轮
         $roller = Category::create(['name' => '滚轮', 'type' => Category::GOODS]);
