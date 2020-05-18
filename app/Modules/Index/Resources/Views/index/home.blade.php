45256e1b0279150a34acd7e90be9c07b1274d14f
WuYu <yu.wu@patpat.com>
2020-05-18 18:57:25 +0800

    home页面任务卡添加权限

--- app/Modules/Index/Resources/Views/index/home.blade.php
+++ app/Modules/Index/Resources/Views/index/home.blade.php
@@ -6,6 +6,7 @@
 @endsection
 @section('content')
     <div class="layui-row layui-col-space15" id="tasks">
+        @can('egress_finished')
         <div class="layui-col-xs3">
             <a lay-href="{{route('warehouse::egress.index')}}" lay-text="出库管理">
                 <div class="layui-card">
@@ -18,6 +19,8 @@
                 </div>
             </a>
         </div>
+        @endcan
+        @can('entry')
         <div class="layui-col-xs3">
             <a lay-href="{{route('warehouse::entry.index')}}" lay-text="入库管理">
                 <div class="layui-card">
@@ -30,6 +33,8 @@
                 </div>
             </a>
         </div>
+        @endcan
+        @can('sale_return_handle')
         <div class="layui-col-xs3">
             <a lay-href="{{route('warehouse::saleReturn.index')}}" lay-text="销售退货管理">
                 <div class="layui-card">
@@ -42,6 +47,8 @@
                 </div>
             </a>
         </div>
+        @endcan
+        @can('add_purchase_order')
         <div class="layui-col-xs3">
             <a lay-href="{{route('warehouse::stockout.index')}}" lay-text="备货管理">
                 <div class="layui-card">
@@ -54,6 +61,8 @@
                 </div>
             </a>
         </div>
+        @endcan
+        @can('review_order')
         <div class="layui-col-xs3">
             <a lay-href="{{route('sale::order.index')}}?status={{\App\Modules\Sale\Models\Order::PENDING_REVIEW}}" lay-text="订单管理">
                 <div class="layui-card">
@@ -66,6 +75,8 @@
                 </div>
             </a>
         </div>
+        @endcan
+        @can('review_return_order')
         <div class="layui-col-xs3">
             <a lay-href="{{route('sale::returnOrder.index')}}?status={{\App\Modules\Sale\Models\ReturnOrder::PENDING_REVIEW}}" lay-text="退货单管理">
                 <div class="layui-card">
@@ -78,6 +89,8 @@
                 </div>
             </a>
         </div>
+        @endcan
+        @can('review_payment_method_application')
         <div class="layui-col-xs3">
             <a lay-href="{{route('sale::paymentMethod.index')}}?status={{\App\Modules\Sale\Models\PaymentMethodApplication::PENDING_REVIEW}}" lay-text="付款方式申请">
                 <div class="layui-card">
@@ -90,5 +103,6 @@
                 </div>
             </a>
         </div>
+        @endcan
     </div>
 @endsection
