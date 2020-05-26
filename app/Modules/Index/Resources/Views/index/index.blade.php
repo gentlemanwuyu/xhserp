27bd0fddd849021b47fa08402e291e191bf831fe
WuYu <yu.wu@patpat.com>
2020-05-26 20:23:31 +0800

    重写导航定位方法

--- app/Modules/Index/Resources/Views/index/index.blade.php
+++ app/Modules/Index/Resources/Views/index/index.blade.php
@@ -344,7 +344,77 @@
         base: '/assets/layuiadmin/' //静态资源所在路径
     }).extend({
         index: 'lib/index' //主入口模块
-    }).use('index');
+    }).use(['index', 'admin', 'view'], function () {
+        var admin = layui.admin
+            ,$ = layui.jquery
+        //监听选项卡切换，改变菜单状态(重写)
+        admin.on('tabsPage(setMenustatus)', function(router){console.log(34);
+            var pathURL = router.url, getData = function(item){
+                    return {
+                        list: item.children('.layui-nav-child')
+                        ,a: item.children('*[lay-href]')
+                    }
+                }
+                ,SIDE_MENU = 'LAY-system-side-menu'
+                ,sideMenu = $('#'+ SIDE_MENU)
+                ,SIDE_NAV_ITEMD = 'layui-nav-itemed'
+                ,THIS = 'layui-this'
+
+                //捕获对应菜单
+                ,matchMenu = function(list){
+                    list.each(function(index1, item1){
+                        var othis1 = $(item1)
+                            ,data1 = getData(othis1)
+                            ,listChildren1 = data1.list.children('dd')
+                            ,matched1 = pathURL === data1.a.attr('lay-href');
+
+                        listChildren1.each(function(index2, item2){
+                            var othis2 = $(item2)
+                                ,data2 = getData(othis2)
+                                ,listChildren2 = data2.list.children('dd')
+                                ,matched2 = pathURL === data2.a.attr('lay-href');
+
+                            listChildren2.each(function(index3, item3){
+                                var othis3 = $(item3)
+                                    ,data3 = getData(othis3)
+                                    ,matched3 = pathURL === data3.a.attr('lay-href');
+
+                                if(matched3){
+                                    var selected = data3.list[0] ? SIDE_NAV_ITEMD : THIS;
+                                    othis3.addClass(selected).siblings().removeClass(selected); //标记选择器
+                                    return false;
+                                }
+
+                            });
+
+                            if(matched2){
+                                matched1 = true;
+                                var selected = data2.list[0] ? SIDE_NAV_ITEMD : THIS;
+                                othis2.addClass(selected).siblings().removeClass(selected); //标记选择器
+                                return false
+                            }
+
+                        });
+
+                        if(matched1){
+                            var selected = data1.list[0] ? SIDE_NAV_ITEMD : THIS;
+                            othis1.addClass(selected).siblings().removeClass(selected); //标记选择器
+                            return false;
+                        }
+
+                    });
+                }
+
+            //重置状态
+            sideMenu.find('.'+ THIS).removeClass(THIS);
+
+            //移动端点击菜单时自动收缩
+            if(admin.screen() < 2) admin.sideFlexible();
+
+            //开始捕获
+            matchMenu(sideMenu.children('li'));
+        });
+    });
 </script>
 </body>
 </html>
